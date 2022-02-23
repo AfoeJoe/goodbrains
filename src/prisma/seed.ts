2bebb269f1027ecdf47e587a37ae399d1f841c31
Joshua Okunola <joshua.okunola@osinit.com>
2022-02-23 23:23:27 +0300

    .

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -4,50 +4,50 @@ const prismaClient = new PrismaClient({});
 
 // A `seed` function so that you can use async/await
 async function seed() {
-	// Create user, posts, and categories
+  // Create user, posts, and categories
 
-	const question1 = await prismaClient.questions.create({
-		data: {
-			question: 'What si life',
-			options: ['I don;t know', 'Guees'],
-			correctAnswer: 1,
-			subject: 'ENGLISH',
-		},
-	});
-	const user = await prismaClient.user.create({
-		data: {
-			email: 'ariadne2@prisma.io',
-			QuestionsMeta: {
-				create: [
-					{
-						exam: 'UNDERTERMINED',
-						year: new Date(),
-						totalAppearances: 0,
-						average: 0,
-						question: {
-							create: {
-								question: 'What si life',
-								options: ['I don;t know', 'Guees'],
-								correctAnswer: 1,
-							},
-						},
-					},
-				],
-			},
-			Attempts: {
-				create: [
-					{
-						questionId: [question1.id],
-						userAnswers: [0],
-						result: 1000,
-					},
-				],
-			},
-		},
-	});
+  const question1 = await prismaClient.questions.create({
+    data: {
+      question: 'What si life',
+      options: ['I don;t know', 'Guees'],
+      correctAnswer: 1,
+      subject: 'ENGLISH',
+    },
+  });
+  const user = await prismaClient.user.create({
+    data: {
+      email: 'ariadne2@prisma.io',
+      QuestionsMeta: {
+        create: [
+          {
+            exam: 'UNDERTERMINED',
+            year: new Date(),
+            totalAppearances: 0,
+            average: 0,
+            question: {
+              create: {
+                question: 'What si life',
+                options: ['I don;t know', 'Guees'],
+                correctAnswer: 1,
+              },
+            },
+          },
+        ],
+      },
+      Attempts: {
+        create: [
+          {
+            questionId: [question1.id],
+            userAnswers: [0],
+            result: 1000,
+          },
+        ],
+      },
+    },
+  });
 
-	// Return user, and posts, and categories
-	/* const returnUser = await prismaClient.user.findUnique({
+  // Return user, and posts, and categories
+  /* const returnUser = await prismaClient.user.findUnique({
 		where: {
 			id: user.id,
 		},
@@ -60,7 +60,7 @@ async function seed() {
 		},
 	}); */
 
-	// console.log(returnUser);
+  // console.log(returnUser);
 }
 
 seed();
